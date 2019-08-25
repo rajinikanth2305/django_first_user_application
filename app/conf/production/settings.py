628457b6babd581c417317863c30648cbe24f92d
rajinikanth2305 <rajinikanthreddyvarala@gmail.com>
2019-08-25 21:08:51 +0530

    static files updated

--- app/conf/production/settings.py
+++ app/conf/production/settings.py
@@ -132,8 +132,8 @@ LANGUAGES = [
 TIME_ZONE = 'UTC'
 USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#STATIC_ROOT = os.path.join(CONTENT_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(CONTENT_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(CONTENT_DIR, 'media')
