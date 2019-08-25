67fe24990cf5ae6baeaedc90c5578a41ed980877
rajinikanth2305 <rajinikanthreddyvarala@gmail.com>
2019-08-25 15:35:33 +0530

    New changes done for allowed host settings

--- app/conf/production/settings.py
+++ app/conf/production/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [
-    'mighty-harbor-45658.herokuapp.com','127.0.0.1'
+    "mighty-harbor-45658.herokuapp.com","127.0.0.1"
 ]
 
 SITE_ID = 1
