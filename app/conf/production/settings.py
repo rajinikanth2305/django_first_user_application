0a7a818b3b24b0706869611034e8d28b835fbf5e
rajinikanth2305 <rajinikanthreddyvarala@gmail.com>
2019-08-25 15:12:02 +0530

    Update ALLOWED_HOSTS with site and development server URL

--- app/conf/production/settings.py
+++ app/conf/production/settings.py
@@ -14,8 +14,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [
-    "li2059-168.members.linode.com"
-
+    'mighty-harbor-45658.herokuapp.com','127.0.0.1'
 ]
 
 SITE_ID = 1
